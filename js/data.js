70cb3f75d1eb597c50e4c75d89e6087977532da1
mikimus986 <mikimus9@email.cz>
2026-08-30 13:52:18 +0200

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,1 +1,33 @@
+let stopsData = {};
+let vehiclesData = {};
+let lineVehiclesData = {};
 
+let lineData = {};
+
+
+async function loadJSON(path) {
+
+    const response = await fetch(path);
+
+    if (!response.ok) {
+        throw new Error("Nelze načíst " + path);
+    }
+
+    return await response.json();
+}
+
+
+async function loadBaseData() {
+
+    stopsData = await loadJSON("data/stops.json");
+
+    vehiclesData = await loadJSON("data/vehicles.json");
+
+    lineVehiclesData =
+        await loadJSON("data/line-vehicles.json");
+
+
+    lineData["1"] =
+        await loadJSON("data/lines/1.json");
+
+}
